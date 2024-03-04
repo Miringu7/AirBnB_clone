ae927c5f03603534d38580360fd39bf012236bc6
miringu7 <miringusimeon@gmail.com>
2024-03-04 19:11:56 +0300

    fixing errors

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,23 @@ from datetime import datetime
 
 
 class BaseModel:
+    """defines all common attributes/methods for other classes:"""
     def __init__(self, *args, **kwargs):
-    """Initialization of Base class model"""
-    self.id = str(uuid.uuid4()
-    self.created_at = datetime.now()
-    self.updated_at = daetime.now()
+        """Initialization of Base class model."""
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.created_at = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns representation of Base Model"""
