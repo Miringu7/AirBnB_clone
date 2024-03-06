9590fc2f654ee3e09953887d3dbdce8c66a7e126
mathewkoech <mathewkoech55@gmail.com>
2024-03-06 11:13:05 -0800

    console structure

---
+++ console.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
+import cmd
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command line interpreter.
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Does nothing when receives an empty line."""
+        pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel, save it, and print its id."""
+        if not arg:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(arg[0])().id)
+            storage.save()
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
