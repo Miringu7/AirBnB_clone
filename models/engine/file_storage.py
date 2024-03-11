ffa486c3b672e2870687dbf939c32a63f0728413
miringu7 <miringusimeon@gmail.com>
2024-03-11 22:26:09 +0300

    fixing errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,25 +18,25 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id."""
-        FileStorage.__objects
+        self.__objects
         ["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)."""
-        with open(FileStorage.__file_path, mode="w") as f:
+        with open(self.__file_path, mode="w") as f:
             dictt = {}
-            for x, y in FileStorage.__objects.items():
+            for x, y in self.__objects.items():
                 dictt[x] = y.to_dict()
             json.dump(dictt, f)
 
     def reload(self):
         """deserializes the JSON file to __objects if file exists."""
         try:
-            with open(FileStorage.__file_path, encoding="utf-8") as f:
+            with open(self.__file_path, encoding="utf-8") as f:
                 for obj in json.load(f).values():
                     self.new(eval(obj["__class__"])(**obj))
         except FileNotFoundError:
