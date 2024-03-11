b227ff330d5952befab6a08b5a5d7c27561b8ce3
miringu7 <miringusimeon@gmail.com>
2024-03-11 23:56:27 +0300

    fixed the "all User" issue

--- console.py
+++ console.py
@@ -48,6 +48,27 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
     def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
@@ -115,18 +136,15 @@ class HBNBCommand(cmd.Cmd):
         else:
             alllist = []
             for val in storage.all().values():
-                print(val)
                 if len(args) > 0 and args[0] == val.__class__.__name__:
                     alllist.append(val.__str__())
-                    print(alllist)
                 elif not arg:
                     alllist.append(val.__str__())
-                    print(alllist)
+            print(alllist)
 
 
     def do_count(self, arg):
         """Usage: count <class> or <class>.count()"""
-        args = arg.split(" ")
         count = 0
         for val in storage.all().values():
             if arg[0] == val.__class__.__name__:
