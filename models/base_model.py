7d52059011c0833a9e35d2b616816e3ba665c114
mathewkoech <mathewkoech55@gmail.com>
2024-03-11 07:34:24 -0700

    class BaseModel that defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -8,22 +8,21 @@ import models
 class BaseModel:
     """defines all common attributes/methods for other classes:"""
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            if 'created_at' in kwargs:
-                self.created_at = (datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"))
-            else:
-                self.created_at = datetime.now()
-            if 'updated_at' in kwargs:
-                self.updated_at = (datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"))
-            else:
-                self.updated_at = datetime.now()
+        """Initialization of Base class model"""
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.created_at = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """Returns representation of Base Model"""
